2b3c74e6847be62917dd65f9c198a438b78cd581
Emelie572 <wahlestedt.emelie@gmail.com>
2025-02-02 14:05:08 +0100

    Repository added + getCustomers()

--- src/Repository/Repository.java
+++ src/Repository/Repository.java
@@ -12,7 +12,6 @@ public class Repository {
 
         List<Customer> getCustomers() throws IOException, SQLException {
 
-
         Properties p = new Properties();
         p.load(new FileInputStream("src/settings.properties"));
 
